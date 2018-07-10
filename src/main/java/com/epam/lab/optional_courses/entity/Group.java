a49973441b9767db408c441715345bfe217b13e7
Ilia_Malyshev <r_alliance@mail.ru>
2018-07-10 15:33:18 +0300

    Task #1. Add class "Group" to entity. @MalyshevI

---
+++ src/main/java/com/epam/lab/optional_courses/entity/Group.java
@@ -0,0 +1,57 @@
+package com.epam.lab.optional_courses.entity;
+
+import java.util.Objects;
+
+/**
+ * The class describes groups, including students from one class.
+ *
+ * @author Ilia_Malyshev
+ */
+public class Group {
+    private int id;
+    private User head;
+    private String groupName;
+
+    public Group(int id, User head, String groupName) {
+        this.id = id;
+        this.head = head;
+        this.groupName = groupName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getHead() {
+        return head;
+    }
+
+    public void setHead(User head) {
+        this.head = head;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return id == group.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
