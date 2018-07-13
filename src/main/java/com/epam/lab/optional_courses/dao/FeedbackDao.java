00b553b4bc6c3b0d2aeddb666cb6914bd3deac9d
Kulaga Anton <kindet27@gmail.com>
2018-07-13 12:56:43 +0300

    Adding feedback DAO interfaces

--- src/main/java/com/epam/lab/optional_courses/dao/FeedbackDao.java
+++ src/main/java/com/epam/lab/optional_courses/dao/FeedbackDao.java
@@ -1,17 +1,23 @@
 package com.epam.lab.optional_courses.dao;
 
+import com.epam.lab.optional_courses.entity.Course;
 import com.epam.lab.optional_courses.entity.Feedback;
 
 import java.util.List;
 
 public interface FeedbackDao {
 
-    Feedback getFeedbackById(int feedbackId);
+    Feedback getFeedbackByUserAndCourse(User user, Course course);
 
-    List<Feedback> getFeedbacksByUserId(int userId);
+    List<Feedback> getFeedbacksByUser(User user);
 
-    List<Feedback> getFeedbacksByTutorId(int tutorId);
+    List<Feedback> getFeedbacksByCourse(Course course);
 
     List<Feedback> getAllFeedbacks();
 
+    boolean addFeedback (Feedback feedback);
+
+    boolean deleteFeedback (Feedback feedback);
+
+    Feedback updateFeedback (Feedback feedback);
 }
